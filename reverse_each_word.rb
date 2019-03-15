9484943349fcb3627680f803a29d501c0e3df1d8
Christopher Luna <chris@illusionlayervfx.com>
2019-03-15 19:55:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence1)
   reversed_sentence = []
   reverse = []
   array_words = sentence1.split(" ")
-    array_words.collect! do |word|
+    array_words.collect do |word|
     reversed_sentence = word.reverse
 
     reverse << reversed_sentence
